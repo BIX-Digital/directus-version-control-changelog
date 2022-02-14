a981526615ad2a2917b753feb51f438257c9b44c
Manuel Feller <manuel.feller@boehringer-ingelheim.com>
2022-02-14 15:39:52 +0100

    making mocks more lean

--- tests/ExceptionProvider.test.ts
+++ tests/ExceptionProvider.test.ts
@@ -2,39 +2,35 @@ import 'jest';
 import ExceptionProvider from '../src/core/ExceptionProvider';
 import { ExceptionTypes } from '../src/enums/ExceptionTypes';
 
+// mocks for the exceptions
 class BaseException extends Error {
-	status: number;
-	code: string;
 	extensions: Record<string, any>;
-	constructor(message: string, status: number, code: string, extensions?: Record<string, any>) {
+	constructor(message: string) {
 		super(message);
-		this.status = status;
-		this.code = code;
-		this.extensions = extensions || {};
 	}
 }
 class InvalidConfigException extends BaseException {
 	constructor(message = 'Invalid config') {
-		super(message, 503, 'INVALID_CONFIG');
+		super(message);
 	}
 }
 class InvalidCredentialsException extends BaseException {
 	constructor(message = 'Invalid user credentials.') {
-		super(message, 401, 'INVALID_CREDENTIALS');
+		super(message);
 	}
 }
 class UnexpectedResponseException extends BaseException {
 	constructor(message: string) {
-		super(message, 503, 'UNEXPECTED_RESPONSE');
+		super(message);
 	}
 }
 class FailedValidationException extends BaseException {
 	constructor(message: string) {
-		super(message, 400, 'FAILED_VALIDATION');
+		super(message);
 	}
 }
 
-describe('Environment', () => {
+describe('ExceptionProvider', () => {
 	let instance: ExceptionProvider;
 
 	beforeEach(() => {
@@ -80,6 +76,5 @@ describe('Environment', () => {
 		expect(failed).toBeInstanceOf(FailedValidationException);
 		expect(failed.message).toBe('f');
 	});
-
 
 });
